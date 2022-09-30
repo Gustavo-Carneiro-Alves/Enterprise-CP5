3338886170bc0ff5b57d3077bfdddb6fe25fdf44
Gustavo-Carneiro-Alves <80040530+Gustavo-Carneiro-Alves@users.noreply.github.com>
2022-09-29 21:35:22 -0300

    Add files via upload

---
+++ Nova pasta/WebApplication1/Models/ClienteTeste.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class ClienteTeste
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
---
+++ Nova pasta/WebApplication1/Models/Produto.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Name { get; set; }
+    }
+    public enum Estado
+    {
+        SP,
+        RJ,
+        MG
+    }
+
+    public class Produto
+    {
+        [HiddenInput]
+        public int ProdutoId { get; set; }
+        public string Name { get; set; } //
+        public string Descricao { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; } //
+        public decimal Valor { get; set; }
+        public Categoria Categoria { get; set; } //
+        public int? CategoriaId { get; set; }
+        public Estado Estado { get; set; } //
+        public bool Importado { get; set; } //
+    }
+
+}
